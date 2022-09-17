e7d755a7e44c09c71ddc114f1277c69133760eac
rtmtorresmorales <ratorres@ponce.inter.edu>
2022-09-17 19:35:00 -0400

    Add files via upload

---
+++ series.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Proyect Paart 1 7.14 Ramon Torres Sept 17 2022
+
+
+# In[3]:
+
+
+import pandas as pd
+
+
+# In[4]:
+
+
+grades = pd.Series([87, 100, 94])
+
+
+# In[5]:
+
+
+grades = pd.Series({'Wally': 87, 'Eva': 100, 'Sam': 94})
+
+
+# In[6]:
+
+
+grades
+
+
+# In[7]:
+
+
+grades ['Eva']
+
+
+# In[8]:
+
+
+grades.Wally
+
+
+# In[10]:
+
+
+grades.values
+
+
+# In[ ]:
+
+
+# Ramon Torres
+
